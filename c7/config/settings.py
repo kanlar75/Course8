994b2d52ebe5885ebc9eead9497c28b6a9ea67b8
kanlar75 <kanlar75@gmail.com>
2023-11-21 18:29:09 +0300

    fix settings

--- c7/config/settings.py
+++ c7/config/settings.py
@@ -72,9 +72,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 if ENV_TYPE == 'local':
-    h = '127.0.0.1'
+    host = '127.0.0.1'
 else:
-    h = os.getenv('POSTGRES_HOST')
+    host = os.getenv('POSTGRES_HOST')
 
 DATABASES = {
     'default': {
@@ -83,7 +83,7 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'PORT': os.getenv('POSTGRES_PORT'),
-        'HOST': h,
+        'HOST': host,
 
     }
 }
