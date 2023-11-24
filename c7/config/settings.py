651081639c7f8ad2af4cdd8e148e6b4c847816bd
kanlar75 <kanlar75@gmail.com>
2023-11-24 19:22:13 +0300

    settings.py datetime format

--- c7/config/settings.py
+++ c7/config/settings.py
@@ -131,7 +131,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-DATETIME_FORMAT = "%d.%m.%Y %H:%M"
+# DATETIME_FORMAT = "%d.%m.%Y %H:%M"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
